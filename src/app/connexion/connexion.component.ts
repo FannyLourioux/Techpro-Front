e1f79b9d40d452c938bba7994f016a79ddb16b96
FannyLourioux <f.lourioux@gmail.com>
2021-03-22 16:09:08 +0100

    chore: login requierment

--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -31,26 +31,3 @@ export class ConnexionComponent implements OnInit {
   }
 
 }
-
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-connexion',
-//   templateUrl: './connexion.component.html',
-//   styleUrls: ['./connexion.component.css']
-// })
-// export class ConnexionComponent implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit(): void {
-//   }
-
-//   connexion(x:any){
-//     let data = x.value;
-//     console.log(data);
-
-    
-//   }
-
-// }
